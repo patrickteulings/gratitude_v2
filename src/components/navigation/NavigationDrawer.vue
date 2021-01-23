e2ec632c125b0572d3b80cb23259fc16c8bcdd54
Patrick Teulings <patrickteulings@gmail.com>
2021-01-23 20:20:54 +0100

    .

--- src/components/navigation/NavigationDrawer.vue
+++ src/components/navigation/NavigationDrawer.vue
@@ -44,7 +44,7 @@
           <li class="navigation__item last">
             <div class="spacer">
               <span class="navigation__icon"><svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" class="feather feather-info"><circle cx="12" cy="12" r="10"></circle><line x1="12" y1="16" x2="12" y2="12"></line><line x1="12" y1="8" x2="12.01" y2="8"></line></svg></span>
-              <a class="navigation__link" @click.prevent="navigateTo('/about')" href="/about">About this wonderfull app</a>
+              <a class="navigation__link" @click.prevent="navigateTo('/about')" href="/about">About this beautifull app</a>
             </div>
           </li>
         </ul>
